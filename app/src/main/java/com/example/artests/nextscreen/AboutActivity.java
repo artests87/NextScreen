86bbb58a16007ed4b04b7b3370862436f8fcd48c
artests <artests87@gmail.com>
2015-09-16 16:56:55 +0300

    Lesson5continue

---
+++ app/src/main/java/com/example/artests/nextscreen/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.example.artests.nextscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by artests on 16.09.2015.
+ */
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
